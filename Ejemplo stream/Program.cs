d457b0362768a742a2f28a8f0a7ae2e88a077065
RamiroMM21 <105456306+RamiroMM21@users.noreply.github.com>
2022-05-31 11:29:33 -0700

    EscrituraArchivos

---
+++ Ejemplo stream/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejemplo_stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Ejemplo.txt");
+            //si el archivo no existe lo creara
+            //si ya existe, escribira en el
+
+            string[] Lines ={
+                "Esta n la informacion de la primera linea",
+                "Esta es la segunda linea",
+                "Fin del tetxto"
+            };
+            foreach (string Line in Lines)
+            {
+                sw.WriteLine(Line);
+            }
+            sw.Close();//se debe cerrare el archivo
+
+            Console.WriteLine("Escrbiendo en el archivo.....");
+            Console.ReadLine();
+        }
+    }
+}
